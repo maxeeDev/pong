93edc01f95aa2f1c26903f40b1563b3d2fe2a382
maxez01 <m.zeband@gmail.com>
2025-07-18 11:05:26 +0200

    Detect ball leaving the arena

--- ball/ball.gd
+++ ball/ball.gd
@@ -1,1 +1,10 @@
 extends RigidBody2D
+
+
+func _on_outside_area_left_body_entered(body: Node2D) -> void:
+	if body.name == "Ball":
+		print("Player2 scored!")
+
+func _on_outside_area_right_body_entered(body: Node2D) -> void:
+	if body.name == "Ball":
+		print("Player1 scored!")
