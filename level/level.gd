4e1bd616fd4640ea1bd92ae16dd536be66108a07
maxez01 <m.zeband@gmail.com>
2025-07-23 16:32:44 +0200

    increase ball speed and implement simple ai

--- level/level.gd
+++ level/level.gd
@@ -3,18 +3,29 @@ extends Node2D
 const INITIAL_BALL_POSITION: Vector2 = Vector2(576,324)
 
 var player1_score: int = 0
-var player2_score: int = 0
+var player2_score: int = 0 
 
 func _on_outside_area_left_body_entered(body: Node2D) -> void:
 	if body.name == "Ball":
 		player2_score += 1
 		$Player2Score.text = str(player2_score)
-		
 		$Ball.should_reset = true
+		$BallMovementLine2D.clear_points()
+		
+		
 
 func _on_outside_area_right_body_entered(body: Node2D) -> void:
 	if body.name == "Ball":
 		player1_score += 1
 		$Player1Score.text = str(player1_score)
-		
 		$Ball.should_reset = true
+		$BallMovementLine2D.clear_points()
+
+
+func update_l2d(points: Array[Vector2]):
+	$BallMovementLine2D.clear_points()
+	$BallMovementLine2D.global_position = $Ball.global_position
+	
+	for point in points:
+		var localized_point = $BallMovementLine2D.to_local(point)
+		$BallMovementLine2D.add_point(localized_point)
--- player/player.gd
+++ player/player.gd
@@ -3,7 +3,8 @@ extends CharacterBody2D
 @export var is_player_1: bool = true
 
 var ball: RigidBody2D
-var direction = 0
+var direction: int = 0
+var ai_target_ypos: float = 100
 
 const SPEED: int = 400
 
@@ -12,14 +13,28 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	if is_player_1:
-		direction = get_direction(1)
-		position.y += direction * SPEED * delta
+		update_player_position(1, delta)
 	elif Globals.game_mode == Globals.mode.PLAYER_VS_PLAYER and not is_player_1:
-		direction = get_direction(2)
-		position.y += direction * SPEED * delta
+		update_player_position(2, delta)
 	else:
-		# TODO fix perfect ai
-		position.y = ball.position.y
+		position.y += get_ai_movement_dir() * SPEED * delta
 
-func get_direction(player_number: int) -> int:
-	return Input.get_axis("player" + str(player_number) + "_up" ,"player" + str(player_number) + "_down")
+func update_player_position(player_number: int, delta: float) -> void:
+	var direction = Input.get_axis("player" + str(player_number) + "_up" ,"player" + str(player_number) + "_down")
+	position.y += direction * SPEED * delta
+
+# helpful video for pong ai: https://www.youtube.com/watch?v=GRgYRvv2mPY
+
+func get_ai_movement_dir() -> int:
+	var dist_to_target: float = abs(ai_target_ypos - global_position.y)
+	var accuracy_dist: float = 50
+	
+	ai_target_ypos = ball.global_position.y
+	
+	if (dist_to_target > accuracy_dist):
+		if ai_target_ypos > global_position.y:
+			return 1
+		else:
+			return -1
+	else:
+		return 0
